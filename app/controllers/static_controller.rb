4bf4ea2185f368fd2c6d00f0f70bed1cbfb831ed
Tom White <twar59@twar59-xu.(none)>
2010-01-23 16:14:41 -0700

    add youtube video and thumb, also add some navigation

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def index
-
+    # TODO: This does not handle zero videos
     @videos = Video.featured APP_CONFIG[:pool_size]
     @current_video = @videos[rand( [APP_CONFIG[:pool_size], @videos.size].min )]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
 [User, Video].each(&:delete_all)    
 
-admin = User.create(:name=>'Admin', :username=>'admin', :email=>'admin@foo.com', :bio=>'I am Admin', :password=>'pass123', :password_confirmation=>'pass123', :is_admin=>true)
+admin = User.create(:name=>'Admin', :username=>'admin', :email=>'admin@foo.com', :bio=>'I am Admin', :password=>'pass123', :password_confirmation=>'pass123')
+admin.is_admin = true # this can not be mass assigned
 admin.save!
 
 joe = User.new(:name=>'Joe', :username=>'joe', :email=>'joe@foo.com', :bio=>'I am Joe', :password=>'monkey', :password_confirmation=>'monkey', :is_admin=>false)
-joe.videos.build :youtube_id=>"asdf1asdf", :title=>"fake movie1", :description=>"This is a fake movie ..."
-joe.videos.build :youtube_id=>"asdf2asdf", :title=>"fake movie2", :description=>"This is a fake movie ..."
-joe.videos.build :youtube_id=>"asdf3asdf", :title=>"fake movie3", :description=>"This is a fake movie ..."
-joe.videos.build :youtube_id=>"asdf4asdf", :title=>"fake movie4", :description=>"This is a fake movie ..."
+joe.videos.build :video_id=>"qseZkggobD4", :title=>"Learning to snowboard", :description=>"This is a fake movie ..."
+joe.videos.build :video_id=>"R7vb6cuP6GE", :title=>"How to go fast", :description=>"This is a fake movie ..."
+joe.videos.build :video_id=>"44KenqMrqI8", :title=>"How to carve", :description=>"This is a fake movie ..."
+joe.videos.build :video_id=>"asdf4asdf", :title=>"fake movie4", :description=>"This is a fake movie ..."
 joe.save!
